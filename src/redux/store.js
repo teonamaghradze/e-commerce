4990378954fe9071b81d7fbee2639979df0b9a61
Teona Maghradze <maghradzeteo@gmail.com>
2024-02-18 17:47:48 +0400

    add redux persist configuration

--- src/redux/store.js
+++ src/redux/store.js
@@ -1,5 +1,15 @@
 import { configureStore } from "@reduxjs/toolkit";
 import productsReducer from "./productsSlice";
+import { storage } from "redux-persist";
+import persistReducer from "redux-persist/es/persistReducer";
+
+const persistConfig = {
+  key: "root",
+  version: 1,
+  storage,
+};
+
+const persistedReducer = persistReducer(persistConfig);
 
 export const store = configureStore({
   reducer: {
